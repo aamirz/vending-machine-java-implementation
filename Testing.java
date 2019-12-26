171e999e3e5d6ab6f00e5594ed5e214c7645e748
Aamir Zainulabadeen <azainulabadeen@gmail.com>
2019-12-26 00:26:39 -0600

    setup testing infrastructure

---
+++ Testing.java
@@ -0,0 +1,11 @@
+package vending_machine_solution;
+
+public class Testing {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Testing vending machine...");
+    }
+
+}
--- makefile
+++ makefile
@@ -1,9 +1,16 @@
-java:
+make:
+	make compile
+
+compile:
 	javac -d .  *.java
 
 
 run:
 	java vending_machine_solution.Machine
+
+
+test:
+	java vending_machine_solution.Testing
 
 
 clean:
